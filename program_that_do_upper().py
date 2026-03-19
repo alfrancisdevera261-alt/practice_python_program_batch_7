529d2b9f9eb004941e7296dbfcdc551532e71096
Alfrancis C. De Vera <alfrancidevera261@gmail.com>
2026-03-19 16:08:05 +0800

    added a function that check if the characters are in the lowercase then turn them into uppercase

---
+++ program_that_do_upper().py
@@ -0,0 +1,9 @@
+text = input("Enter a text: ")
+modified_character = ""
+
+
+for characters in text:
+    if characters.islower():
+        modified_character += characters.swapcase()
+    else:
+        modified_character += characters
