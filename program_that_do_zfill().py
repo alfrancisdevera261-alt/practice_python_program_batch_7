f3070f36040a25b881d75391c67b569409d47791
Alfrancis C. De Vera <alfrancidevera261@gmail.com>
2026-03-19 18:36:08 +0800

    added a if function that check the number of total digit so that it can know how many zero it needed

--- program_that_do_zfill().py
+++ program_that_do_zfill().py
@@ -2,3 +2,9 @@ number = input("Enter a number")
 digit = int(input("How many digits of number: "))
 
 total_digits = digit - len(number)
+
+if total_digits > 0:
+    number_modified = "0" * total_digits + number
+else:
+    number_modified = number
+print(number_modified)
