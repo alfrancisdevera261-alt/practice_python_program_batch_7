548a239f3cd009f0b7abde360f6ab7b9090162fa
Alfrancis C. De Vera <alfrancidevera261@gmail.com>
2026-03-19 16:16:35 +0800

    forgot to put () in the isupper

--- program_that_do_islower().py
+++ program_that_do_islower().py
@@ -2,7 +2,7 @@ text = input("Enter a text: ")
 is_all_lower = True
 
 for characters in text:
-    if characters.isupper:
+    if characters.isupper():
         is_all_lower = False
     else:
         is_all_lower = True
