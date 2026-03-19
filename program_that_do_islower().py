fe4f50a8d854e9913c1bd5d83936a17fc5c61e44
Alfrancis C. De Vera <alfrancidevera261@gmail.com>
2026-03-19 16:15:26 +0800

    added a boolean to know if the characters is all in lower case

--- program_that_do_islower().py
+++ program_that_do_islower().py
@@ -1,5 +1,9 @@
 text = input("Enter a text: ")
+is_all_lower = True
 
 for characters in text:
     if characters.isupper:
-        
+        is_all_lower = False
+    else:
+        is_all_lower = True
+print(is_all_lower)
