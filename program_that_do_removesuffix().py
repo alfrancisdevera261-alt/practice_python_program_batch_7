80e6398ef6abea077cd4e0259608cc07f4c05262
Alfrancis C. De Vera <alfrancidevera261@gmail.com>
2026-03-19 16:03:34 +0800

    added a function that check the same len to the text and then check if they are the same then it will be modified to replace the suffix with empty string

--- program_that_do_removesuffix().py
+++ program_that_do_removesuffix().py
@@ -1,2 +1,8 @@
 text = input("Enter a text: ")
 suffix = input("Enter the suffix: ")
+
+if text[-len(suffix):] == suffix:
+    text_modified = text.replace(suffix, "", 1)
+    print(text_modified)
+else:
+    print("Does not exist")
