ffeec8978bab94cd8420b7eee2e8a0e05f445ef3
Alfrancis C. De Vera <alfrancidevera261@gmail.com>
2026-03-19 18:30:05 +0800

    added a if function to check the total width and add the space of how many needed

--- progra_that_do_rjust().py
+++ progra_that_do_rjust().py
@@ -2,3 +2,9 @@ text = input("ENter a text: ")
 width = int(input("Enter the desired width: "))
 
 total_width = width - len(text)
+
+if total_width > 0:
+    modified_text = " " * total_width + text
+else:
+    modified_text = text
+print(modified_text)
