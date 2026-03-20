69a6c393a09888a1fad4ede417c8ac5d05d10d72
Alfrancis C. De Vera <alfrancidevera261@gmail.com>
2026-03-20 20:14:49 +0800

    changes in how getting the index now by getting the index from characters

--- program_that_do_rindex().py
+++ program_that_do_rindex().py
@@ -3,10 +3,10 @@ substring = input("Enter the substring: ")
 
 index_count = 0
 
-for characters in range(len(text) -1, -1, -1):
-    if text[characters:characters+len(substring)] != substring:
-        index_count = (len(text)-1) - 1
-    else:
+for characters in range(len(text)-1, -1, -1):
+    if text[characters:characters+len(substring)] == substring:
+        index_count = characters
         print(index_count)
         break
-print(len(text))
+    else:
+        print("There is no match")
