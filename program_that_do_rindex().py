fb7d882fc04d4e45ec42218f72d569d8ac52fee7
Alfrancis C. De Vera <alfrancidevera261@gmail.com>
2026-03-20 19:50:07 +0800

    added for loop to check the characters in the text and count the index  until it finds the substring

--- program_that_do_rindex().py
+++ program_that_do_rindex().py
@@ -1,2 +1,11 @@
 text = input("Enter a text: ")
 substring = input("Enter the substring: ")
+
+index_count = 0
+
+for characters in range(len(text) -1, -1, -1):
+    if text[characters:characters+len(substring)] != substring:
+        index_count = len(text) - 1
+    else:
+        print(index_count)
+        break
