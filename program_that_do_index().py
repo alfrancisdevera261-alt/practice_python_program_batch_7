13ad0e0e95910501898a0e3a17c6f5bbd37dab1e
Alfrancis C. De Vera <alfrancidevera261@gmail.com>
2026-03-20 19:40:54 +0800

    added a for loop that check the index and the subtring is its the same

--- program_that_do_index().py
+++ program_that_do_index().py
@@ -1,2 +1,11 @@
 text = input("Enter a text: ")
 substring = input("Enter the substring: ")
+
+index_count = 0
+
+for characters in range(len(text)):
+    if text[characters:characters+len(substring)] != substring:
+        index_count += 1
+    else:
+        print(index_count)
+        break
