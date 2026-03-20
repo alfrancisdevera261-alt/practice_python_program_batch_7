ced960fd41e1c45e89cdccfd1db4b04ffa1640b9
Alfrancis C. De Vera <alfrancidevera261@gmail.com>
2026-03-20 19:52:42 +0800

    added the -1 to the len to get the index of the text

--- program_that_do_rindex().py
+++ program_that_do_rindex().py
@@ -5,7 +5,8 @@ index_count = 0
 
 for characters in range(len(text) -1, -1, -1):
     if text[characters:characters+len(substring)] != substring:
-        index_count = len(text) - 1
+        index_count = (len(text)-1) - 1
     else:
         print(index_count)
         break
+print(len(text))
